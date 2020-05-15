25b5a5c9326906327361efcb5f13d353905030f1
levee98 <leventet@t-email.hu>
2020-05-15 06:22:43 +0200

    first sentence sumArray

---
+++ sumArray.js
@@ -0,0 +1,11 @@
+// 1. tétel Összegzés tétele
+const sumArray = (arr) => {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+};
+
+const ar = [1, 2, 3, 4, 5, 6];
+console.log(sumArray(ar));
